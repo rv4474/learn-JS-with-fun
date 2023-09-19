892e5672955f0c71403c3da4e9932a0c78c236b0
Rajan verma <113434676+rv4474@users.noreply.github.com>
2023-09-19 17:35:32 +0000

    Variable

---
+++ Basic/01_variables.js
@@ -0,0 +1,6 @@
+const accountId=1223  //can't modified further
+let accountEmail="jdsb@gmail.com"  
+var accountPassword="87654" //having scope issue to overcome this let comes
+accountCity="UP" 
+let accountState   //if we just declared the variable then default value will be undefined
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
