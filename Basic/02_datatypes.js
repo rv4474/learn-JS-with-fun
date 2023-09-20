f63aad979967a560910cd1a50407d01fac472ed3
Rajan verma <113434676+rv4474@users.noreply.github.com>
2023-09-20 04:44:56 +0000

    updated

--- Basic/02_datatypes.js
+++ Basic/02_datatypes.js
@@ -1,5 +1,5 @@
 "use strict"; // treat all JS code as newer version
-
+// ////javscript does not have input funtion
 // alert( 3 + 3) // we are using nodejs, not browser
 let name = "hitesh"
 let age = 18
