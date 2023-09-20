4a3169973d57d80f4aeb0afe8c6ff830bc1b6fa1
Rajan verma <113434676+rv4474@users.noreply.github.com>
2023-09-20 17:44:51 +0000

    addedd

---
+++ Basic/04_comparison.js
@@ -0,0 +1,9 @@
+console.log(null==0)
+console.log(null<0)
+console.log(null>0)
+console.log(null!=0) //true
+console.log(null<=0) //true
+console.log(null>=0) //true
+
+//undefine will give false in all above case
+// === strict check compare datatype too
